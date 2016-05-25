b391245735c7de39c6425f0cff62bab34c870f9e
deepak-initcron <deepakjain@initcron.org>
2016-05-25 19:02:35 +0530

    Revert "adding full name"
    This reverts commit d2c4fde35debd22f1a861c2168d31bd2bca296a9.

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cat >> /etc/hosts << 'EOF'
-10.0.0.2 prx1.uslax.prd.exactnote.com
-10.0.0.3 app1.uslax.prd.exactnote.com
-10.0.0.4 sql1.uslax.prd.exactnote.com
-10.0.0.5 red1.uslax.prd.exactnote.com
+10.0.0.2 prx1
+10.0.0.3 app1
+10.0.0.4 sql1
+10.0.0.5 red1
 EOF
